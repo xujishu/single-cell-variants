2129bfa47d458e719cb3ea97bee00cbe521e1836
xujishu <jishu.xu@gmail.com>
2019-01-15 14:30:46 -0500

    first commit

---
+++ scripts/RunVariantsTenX.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+
+tenXbam=${IN_TENX_BAM}
+barcode=${BARCODE}
+dbsnp=${DBSNP}
+tenx_fa=${TENX_FA}
+output_dir="$(dirname "${OUTPUT_FILES}")"
+samplename=${TENX_SAMPLE}
+# run samtools
+echo "samtools..."
+samtools view -H ${tenXbam} >${output_dir}/${barcode}.sam
+samtools view ${tenXbam} |grep "CB:Z:${barcode}" >>${output_dir}/${barcode}.sam
+samtools view -b  -o ${output_dir}/${barcode}.bam ${output_dir}/${barcode}.sam
+samtools index ${output_dir}/${barcode}.bam
+echo "remote duplciated umi...."
+umi_tools dedup -I ${output_dir}/${barcode}.bam  -S ${output_dir}/${barcode}.dedup.bam --umi-tag="UB" --cell-tag="CB"  --extract-umi-method=tag
+# bcftools variants
+# rename read group to barcode
+echo "Run bcftools....."
+echo  "${samplename} ${barcode}" >${output_dir}/${barcode}_RG.txt
+# raw variants call
+cmd="bcftools mpileup -Ou -S ${output_dir}/${barcode}_RG.txt -f ${tenx_fa} ${output_dir}/${barcode}.dedup.bam|bcftools call -mv -Ob -o ${output_dir}/${barcode}.bcf"
+echo $cmd
+bcftools mpileup -Ou -S ${output_dir}/${barcode}_RG.txt -f ${tenx_fa} ${output_dir}/${barcode}.dedup.bam|bcftools call -mv -Ob -o ${output_dir}/${barcode}.bcf
+bcftools index ${output_dir}/${barcode}.bcf
+# snpEff annotate variants
+echo "annotation....."
+bcftools annotate -c ID -a  ${dbsnp} ${output_dir}/${barcode}.bcf -Ov |java -Xmx7g -jar /opt/snpEff/snpEff.jar ann GRCh38.86 -csvStats ${output_dir}/${barcode}.stat.csv - |bcftools view -Ob - >${output_dir}/${barcode}.annt.bcf
+bcftools index ${output_dir}/${barcode}.annt.bcf
+
+#clean bam and sam
+rm ${output_dir}/${barcode}.bam* ${output_dir}/${barcode}.dedup.bam ${output_dir}/${barcode}.sam
---
+++ scripts/submit_variants.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+readonly PROJECT_ID=${1}
+readonly provider=""google-v2
+readonly logging=${2}
+readonly machine_type="n1-standard-2"
+readonly in_tenx_bam=${3}
+readonly tenx_sample=${4}
+readonly tenx_fa=${5}
+readonly output=${6}
+readonly script=${7}
+readonly snpEffDB="GRCh38.86"
+readonly dbsnp="ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606_b150_GRCh38p7/VCF/00-All.vcf.gz"
+readonly DOCKER="quay.io/xujishu/tenx-variants-call:latest"
+
+while read -r line
+do
+
+  barcode=$(echo $line|awk '{print $1}')
+  counts=$(echo $line|awk '{print $4}')
+  genes=$(echo $line|awk '{print $2}')
+  if [[ $genes -gt 1000 && ${counts%.*} -gt 10000 ]];then
+   
+   outdir=${output}/${barcode}
+   cmd="dsub \
+    --project ${PROJECT_ID} \
+    --zones "us-central1-*" \
+    --provider "${provider}"
+    --logging ${logging} \
+    --disk-size 100 \
+    --boot-disk-size 50 \
+    --machine-type ${machine_type} \
+    --image ${DOCKER} \
+    --script ${script} \
+    --env TENX_SAMPLE=${tenx_sample} \
+    --input IN_TENX_BAM=${in_tenx_bam} \
+    --input IN_TENX_BAM_INDEX=${in_tenx_bam}.bai \
+    --env BARCODE=${barcode} \
+    --env DBSNP=${dbsnp} \
+    --env SNPEFFDB=${snpEffDB} \
+    --input TENX_FA=${tenx_fa} \
+    --input TENX_FAI="${tenx_fa}.fai" \
+    --output OUTPUT_FILES="${outdir}/${barcode}.*"
+    --preemptible "
+   echo $cmd
+   $cmd
+fi
+done
